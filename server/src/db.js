294563a65cdb0849a8434e6d7e2eee102fde6c70
1516604075 <1516604075@qq.com>
2025-09-16 16:26:56 -0700

    Create db.js

---
+++ server/src/db.js
@@ -0,0 +1,17 @@
+import pkg from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const { Pool } = pkg;
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL && process.env.DATABASE_URL.includes('render.com')
+    ? { rejectUnauthorized: false }
+    : false
+});
+
+export async function query(text, params) {
+  const res = await pool.query(text, params);
+  return res;
+}
